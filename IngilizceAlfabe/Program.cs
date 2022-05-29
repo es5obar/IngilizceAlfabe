f87e82e6a3ad99a594df3d47b18ba4771e331e8b
Yahya Önder <06yahya06@gmail.com>
2022-05-29 23:08:43 +0300

    Add project files.

---
+++ IngilizceAlfabe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngilizceAlfabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 65; i <= 90; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+            Console.WriteLine();
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+            for (char c = 'A'; c <= 'z'; c++)
+            {
+                if (c <='Z' || c >= 'a')
+                {
+                    Console.Write(c + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
